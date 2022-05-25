dd498c7f7663ee59695391a08530620f3228fe42
Alan Hayes <ahayes@dotdash.com>
2022-05-25 19:11:08 -0400

    API Gateway

---
+++ api-gw.tf
@@ -0,0 +1,54 @@
+locals {
+  aws_api_gateway_rest_api_name = format("${var.name}-%s", "api-gateway")
+  aws_api_gateway_resource_path_part = var.name
+  aws_api_gateway_method_authorization = "NONE"
+  aws_api_gateway_method_http_method = "POST"
+  aws_api_gateway_integration_type = "MOCK"
+  aws_api_gateway_stage_stage_name = var.name
+}
+
+resource "aws_api_gateway_rest_api" "slack-bot" {
+  name = local.aws_api_gateway_rest_api_name
+}
+
+resource "aws_api_gateway_resource" "slack-bot" {
+  parent_id   = aws_api_gateway_rest_api.slack-bot.root_resource_id
+  path_part   = local.aws_api_gateway_resource_path_part
+  rest_api_id = aws_api_gateway_rest_api.slack-bot.id
+}
+
+resource "aws_api_gateway_method" "slack-bot" {
+  authorization = local.aws_api_gateway_method_authorization
+  http_method   = local.aws_api_gateway_method_http_method
+  resource_id   = aws_api_gateway_resource.slack-bot.id
+  rest_api_id   = aws_api_gateway_rest_api.slack-bot.id
+}
+
+resource "aws_api_gateway_integration" "slack-bot" {
+  http_method = aws_api_gateway_method.slack-bot.http_method
+  resource_id = aws_api_gateway_resource.slack-bot.id
+  rest_api_id = aws_api_gateway_rest_api.slack-bot.id
+  type        = local.aws_api_gateway_integration_type
+}
+
+resource "aws_api_gateway_deployment" "slack-bot" {
+  rest_api_id = aws_api_gateway_rest_api.slack-bot.id
+
+  triggers = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.slack-bot.id,
+      aws_api_gateway_method.slack-bot.id,
+      aws_api_gateway_integration.slack-bot.id,
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "slack-bot" {
+  deployment_id = aws_api_gateway_deployment.slack-bot.id
+  rest_api_id   = aws_api_gateway_rest_api.slack-bot.id
+  stage_name    = local.aws_api_gateway_rest_api_name
+}
---
+++ variable.tf
@@ -0,0 +1,7 @@
+## API Gateway
+
+variable "name" {
+  default = "slackbot"
+  description = "Defualt Name for the Progject"
+  sensitive = false
+}
