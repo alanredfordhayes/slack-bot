1c3539468dac267dc7898afb8fdb1b9212bd3cf9
Alan Hayes <ahayes@dotdash.com>
2022-05-26 13:28:53 -0400

    record pass through

--- db_processor/db_processor.py
+++ db_processor/db_processor.py
@@ -5,7 +5,7 @@ import os
 # DynamDB
 table_name = os.environ['slack_bot_table_name']
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table(table_name)
+db_processor_table = dynamodb.Table(table_name)
 
 def lambda_handler(event, context):
     # TODO implement
@@ -15,12 +15,12 @@ def lambda_handler(event, context):
         table_item = {}
         table_item['EventID'] = record['eventID']
         table_item['awsRegion'] = record['awsRegion']
-        table_item['dynamodb'] = record['dynamodb']['Keys']['EventID']
+        table_item['dynamodb'] = record['dynamodb']['Keys']['EventID']['S']['S']
         table_item['eventName'] = record['eventName']
         table_item['eventSource'] = record['eventSource']
         table_item['eventVersion'] = record['eventVersion']
         
-        table.put_item(
+        db_processor_table.put_item(
             Item = table_item
         )
     
