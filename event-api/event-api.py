1d3db1fc410fedb3603421c0af58c98716b8ca7e
Alan Hayes <ahayes@dotdash.com>
2022-05-25 22:41:12 -0400

    challenge

--- event-api/event-api.py
+++ event-api/event-api.py
@@ -1,8 +1,22 @@
 import json
+from lib2to3.pgen2 import token
 
 def lambda_handler(event, context):
     # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    
+    httpMethod = event['httpMethod']
+    
+    if httpMethod == "POST":
+        body = event['body']
+        body = json.dumps(body)
+        token = body['token']
+        challenge = body['challenge']
+        url_type = body['type']
+        
+    responseObject = {}
+    responseObject['statusCode'] = 200
+    responseObject['headers'] = {}
+    responseObject['headers']['Content-Type'] = 'application/json'
+    responseObject['challenge'] = challenge
+        
+    return responseObject
