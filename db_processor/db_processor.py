0f0fa0144142e8b6d3aa76d59a9ee4e9f75bdffd
Alan Hayes <ahayes@dotdash.com>
2022-05-27 15:14:05 -0400

    logging type

--- db_processor/db_processor.py
+++ db_processor/db_processor.py
@@ -14,11 +14,7 @@ db_processor_table_name = os.environ['db_processor_table']
 db_processor_table = dynamodb.Table(db_processor_table_name)
 
 def lambda_handler(event, context):
-    # TODO implement
-    
     for record in event['Records']:
-        
-        
         table_item = {}
         table_item['EventID'] = record['eventID']
         table_item['awsRegion'] = record['awsRegion']
@@ -26,26 +22,18 @@ def lambda_handler(event, context):
         table_item['eventName'] = record['eventName']
         table_item['eventSource'] = record['eventSource']
         table_item['eventVersion'] = record['eventVersion']
-        
-        event_api_record = event_api_table.get_item(
-            Key={
-                'EventID': table_item['dynamodb']
-            }
-        )
-        
+        event_api_record = event_api_table.get_item(Key={'EventID': table_item['dynamodb']})
         event_api_record_item = event_api_record['Item']
         event_api_record_item_body = event_api_record_item['body']
         event_api_record_item_body = json.loads(event_api_record_item_body)
         event_api_record_item_body_event = event_api_record_item_body['event']
+        event_api_record_item_body_event_text = event_api_record_item_body_event['text']
         
-        
-        item_type = type(event_api_record_item_body_event)
+        item_type = type(event_api_record_item_body_event_text)
         logging.info(item_type)
-        logging.info(event_api_record_item_body_event)
+        logging.info(event_api_record_item_body_event_text)
         
-        db_processor_table.put_item(
-            Item = table_item
-        )
+        db_processor_table.put_item(Item = table_item)
     
     return {
         'statusCode': 200,
