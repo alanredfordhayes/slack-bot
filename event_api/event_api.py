a08f90b38fe0d432177d4362d21b19000404502c
Alan Hayes <ahayes@dotdash.com>
2022-05-28 11:34:02 -0400

    bolt to event_api

--- event_api/event_api.py
+++ event_api/event_api.py
@@ -7,38 +7,45 @@ from slack_bolt.adapter.aws_lambda import SlackRequestHandler
 import logging
 
 # DynamDB
-table_name = os.environ['event_api_table']
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table(table_name)
-
-# BOLT
-app = App(process_before_response=True)
-
-@app.event("app_mention")
-def handle_app_mentions(body, say, logger):
-    logger.info(body)
-    say("What's up?")
-
-def lambda_handler(event, context):
-    
-    httpMethod = event['httpMethod']
-    if httpMethod == 'POST':
-        # body = event['body']
-        # body = json.loads(body)
-        # challenge = body['challenge']
-        # event_id = challenge
-        body = event['body']
-        body = json.loads(body)
-        logging.info(body)
-        event_id = body['event_id']
-        event['EventID'] = event_id
-        table.put_item(Item = event)
-        slack_handler = SlackRequestHandler(app=app)
-        
+# Challenge Message
+def challenge_message(event):
+    body = event['body']
+    body = json.loads(body)
+    challenge = body['challenge']
+    event_id = challenge
     responseObject = {}
     responseObject['statusCode'] = 200
     responseObject['headers'] = {}
     responseObject['headers']['Content-Type'] = 'application/json'
     responseObject['body'] = event_id
-    
+    return responseObject
+
+#DYNAMODB  
+table_name = os.environ['event_api_table']
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(table_name)
+
+def dynamodb_put_item(event):
+    body = event['body']
+    body = json.loads(body)
+    logging.info(body)
+    event_id = body['event_id']
+    event['EventID'] = event_id
+    table.put_item(Item = event)
+
+# BOLT
+app = App(process_before_response=True)
+
+@app.event("app_mention")
+def handle_app_mentions(body, say):
+    event_api_record = table.get_item(Key={'EventID': body['event']['channel']})
+    logging.info(body)
+    say("What's up?")
+
+def lambda_handler(event, context):
+    httpMethod = event['httpMethod']
+    if httpMethod == 'POST':
+        dynamodb_put_item(event)
+        slack_handler = SlackRequestHandler(app=app)
+
     return slack_handler.handle(event, context)
