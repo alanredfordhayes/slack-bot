2d8c06210342f82a1711df327824bf16b9e3a697
Alan Hayes <ahayes@dotdash.com>
2022-05-28 11:06:03 -0400

    bolt to event_api

--- event_api/event_api.py
+++ event_api/event_api.py
@@ -1,16 +1,26 @@
 import json
 import boto3
 import os
-
+import time
+from slack_bolt import App
+from slack_bolt.adapter.aws_lambda import SlackRequestHandler
 import logging
-logging.getLogger().setLevel(logging.INFO)
 
 # DynamDB
 table_name = os.environ['event_api_table']
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(table_name)
 
+# BOLT
+app = App(process_before_response=True)
+
+@app.event("app_mention")
+def handle_app_mentions(body, say, logger):
+    logger.info(body)
+    say("What's up?")
+
 def lambda_handler(event, context):
+    
     httpMethod = event['httpMethod']
     if httpMethod == 'POST':
         # body = event['body']
@@ -22,7 +32,8 @@ def lambda_handler(event, context):
         logging.info(body)
         event_id = body['event_id']
         event['EventID'] = event_id
-        table.put_item(Item = event) 
+        table.put_item(Item = event)
+        slack_handler = SlackRequestHandler(app=app)
         
     responseObject = {}
     responseObject['statusCode'] = 200
@@ -30,4 +41,4 @@ def lambda_handler(event, context):
     responseObject['headers']['Content-Type'] = 'application/json'
     responseObject['body'] = event_id
     
-    return responseObject
+    return slack_handler.handle(event, context)
