2d5fa6e86de1604215f01ee3828d63cf088f3e8e
Alan Hayes <ahayes@dotdash.com>
2022-05-29 13:31:40 -0400

    bolt to event_api

--- event_api/event_api.py
+++ event_api/event_api.py
@@ -39,7 +39,22 @@ app = App(process_before_response=True)
 @app.event("app_mention")
 def handle_app_mentions(event, say):
     channel = event['channel']
-    say(channel)
+    blocks = [
+		{ "type": "header", "text": { "type": "plain_text", "text": "Ticket Help", "emoji": True } },
+		{ "type": "section", "text": { "type": "plain_text", "text": "Here is a list of things that I do for you:", "emoji": True } },
+		{
+			"type": "actions", "elements": [
+				{ "type": "button", "text": { "type": "plain_text", "emoji": True, "text": "Create New Ticket" }, "style": "primary", "value": "click_me_123" },
+				{ "type": "button",	"text": { "type": "plain_text", "emoji": True, "text": "View Ticket Watchers" }, "style": "primary", "value": "click_me_123" },
+				{ "type": "button", "text": { "type": "plain_text", "emoji": True, "text": "View Ticket Status" }, "style": "primary", "value": "click_me_123" },
+				{ "type": "button",	"text": { "type": "plain_text", "emoji": True, "text": "View Ticket Comments" }, "style": "primary", "value": "click_me_123" }
+			]
+		}
+	]
+    say(
+        blocks = blocks,
+        text = "How can I help you?"
+    )
 
 
 def lambda_handler(event, context):
